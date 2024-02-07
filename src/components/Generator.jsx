d7f4a117280ba10bcc433a6f491a259d01fde9af
abuhumayed <ahmedthanjim@hotmail.co.uk>
2024-02-07 12:38:27 +0000

    workout creator

--- src/components/Generator.jsx
+++ src/components/Generator.jsx
@@ -1,5 +1,30 @@
 import React from 'react'
+import SectionWrapper from './SectionWrapper'
+
+function Header(props) {
+  const { index, title, description } = props
+  return (
+    <div>
+      <div>
+        <p>{index}</p>
+        <h4>{title}</h4>
+      </div>
+      <p>{description}</p>
+    </div>
+  )
+}
 
 export default function Generator() {
-  return <div>Generator</div>
+  return (
+    <SectionWrapper
+      header={'generate your workout'}
+      title={["It's", 'Huge', "o'clock"]}
+    >
+      <Header
+        index={'01'}
+        title={'Pick your poison'}
+        description={'Select your workout you wish to use!'}
+      />
+    </SectionWrapper>
+  )
 }
